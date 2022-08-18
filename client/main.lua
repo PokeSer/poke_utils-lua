79a585c325fd55f4f07d8cf3294878e83edc1f1a
Poke <sergamer15@gmail.com>
2022-08-18 14:56:42 +0200

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,3 @@
 RegisterNetEvent('poke_utils:useItem', function(provisionType, interactionType)
-    Citizen.InvokeNative(0xAE72E7DF013AAA61, PlayerPedId(), GetHashKey(provisionType), GetHashKey(interactionType), 1, 0, 0) -- StartTaskItemInteraction
+    Citizen.InvokeNative(0xAE72E7DF013AAA61, PlayerPedId(), provisionType, interactionType, 1, 0, 0) -- StartTaskItemInteraction
 end)
--- config.lua
+++ config.lua
@@ -3,12 +3,12 @@ Config = {}
 Config.ItemToUse = {
     {
         item = "pocket_watch",
-        ProvisionType = "PROVISION_POCKET_WATCH_REUTLINGE",
-        InteractionType = "POCKET_WATCH_INSPECT_UNHOLSTER"
+        ProvisionType = 0x8604D7CD, -- "PROVISION_POCKET_WATCH_REUTLINGE",
+        InteractionType = 0x8F20193A -- "POCKET_WATCH_INSPECT_UNHOLSTER"
     },
     {
         item = "pocket_compass", -- Item name
-        ProvisionType = "PROVISION_PEARSONS_NAVAL_COMPASS",
-        InteractionType = "POCKET_WATCH_INSPECT_UNHOLSTER"
+        ProvisionType = 0xD422DB47, -- "PROVISION_PEARSONS_NAVAL_COMPASS",
+        InteractionType = 0x8F20193A -- "POCKET_WATCH_INSPECT_UNHOLSTER"
     }
 }
