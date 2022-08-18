338da541e63701b0a8d6c8386fb84d06ee201e5e
Poke <sergamer15@gmail.com>
2022-08-18 14:32:08 +0200

    Change native name to invokenative

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,3 @@
 RegisterNetEvent('poke_utils:useItem', function(provisionType, interactionType)
-    StartTaskItemInteraction(PlayerPedId(), GetHashKey(provisionType), GetHashKey(interactionType), 1, 0, 0)
+    Citizen.InvokeNative(0xAE72E7DF013AAA61, PlayerPedId(), GetHashKey(provisionType), GetHashKey(interactionType), 1, 0, 0) -- StartTaskItemInteraction
 end)
