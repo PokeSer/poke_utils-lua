dba5534bf8752402432cdecba621a13bc18bd9f0
Poke <sergamer15@gmail.com>
2022-08-18 21:04:31 +0200

    Fix pocket watch
    Fixed pocket watch not appear, for pocket compass i need more time to fix it.

--- config.lua
+++ config.lua
@@ -3,12 +3,12 @@ Config = {}
 Config.ItemToUse = {
     {
         item = "pocket_watch",
-        ProvisionType = 0x8604D7CD, -- "PROVISION_POCKET_WATCH_REUTLINGE",
-        InteractionType = 0x8F20193A -- "POCKET_WATCH_INSPECT_UNHOLSTER"
+        ProvisionType = `PROVISION_POCKET_WATCH_SILVER`, -- PROVISION_POCKET_WATCH_GOLD or PROVISION_POCKET_WATCH_PLATINUM or PROVISION_POCKET_WATCH_SILVER
+        InteractionType = `POCKET_WATCH_INSPECT_UNHOLSTER`
     },
     {
         item = "pocket_compass", -- Item name
-        ProvisionType = 0xD422DB47, -- "PROVISION_PEARSONS_NAVAL_COMPASS",
-        InteractionType = 0x8F20193A -- "POCKET_WATCH_INSPECT_UNHOLSTER"
+        ProvisionType = `PROVISION_OLD_BRASS_COMPASS`,
+        InteractionType = `POCKET_WATCH_INSPECT_UNHOLSTER`
     }
 }
---
+++ items.sql
@@ -0,0 +1,2 @@
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('pocket_watch', 'Pocket Watch', 10, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('pocket_compass', 'Pocket Compass', 10, 1, 'item_standard', 1);
